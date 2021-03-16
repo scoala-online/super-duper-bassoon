4fb50242c8ff7280aeefdce867457233ce6bd1c7
GiugioiuMarian <38139536+MarianGiugioiu@users.noreply.github.com>
2021-03-16 13:23:14 +0200

    Update apps/java-api/src/main/java/org/scoalaonline/api/JavaApiApplication.java
    Co-authored-by: CristiPV <cristi.valentin29@gmail.com>

--- apps/java-api/src/main/java/org/scoalaonline/api/JavaApiApplication.java
+++ apps/java-api/src/main/java/org/scoalaonline/api/JavaApiApplication.java
@@ -3,7 +3,6 @@ package org.scoalaonline.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.HashSet;
 
 @SpringBootApplication
 public class JavaApiApplication {

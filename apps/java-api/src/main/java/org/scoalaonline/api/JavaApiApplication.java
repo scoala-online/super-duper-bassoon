d882537ac9813e2ebbea9b7831e10a5a46438e7e
GiugioiuMarian <38139536+MarianGiugioiu@users.noreply.github.com>
2021-03-16 13:23:06 +0200

    Update apps/java-api/src/main/java/org/scoalaonline/api/JavaApiApplication.java
    Co-authored-by: CristiPV <cristi.valentin29@gmail.com>

--- apps/java-api/src/main/java/org/scoalaonline/api/JavaApiApplication.java
+++ apps/java-api/src/main/java/org/scoalaonline/api/JavaApiApplication.java
@@ -1,6 +1,5 @@
 package org.scoalaonline.api;
 
-import org.scoalaonline.api.model.Artist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

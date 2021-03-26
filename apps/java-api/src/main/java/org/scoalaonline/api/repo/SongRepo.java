5df88e41c27e39bcd7e2ea6f23540d127f08e07f
Marian <wyz_marian_013@yahoo.com>
2021-03-26 14:51:53 +0200

    refactor: 💡 delete test files

--- apps/java-api/src/main/java/org/scoalaonline/api/repo/SongRepo.java
+++
@@ -1,7 +0,0 @@
-package org.scoalaonline.api.repo;
-
-import org.scoalaonline.api.model.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepo extends JpaRepository<Song, Long>{
-}

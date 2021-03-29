c7a56698f8311ab61e9887c095312face7e5706d
Marian <wyz_marian_013@yahoo.com>
2021-03-29 11:18:30 +0300

    feat: 🎸 create get controller mappings

---
+++ apps/java-api/src/main/java/org/scoalaonline/api/controller/ArtistController.java
@@ -0,0 +1,34 @@
+package org.scoalaonline.api.controller;
+
+import org.scoalaonline.api.exception.ResourceNotFoundException;
+import org.scoalaonline.api.model.Artist;
+import org.scoalaonline.api.service.ArtistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/artist")
+public class ArtistController {
+  @Autowired
+  ArtistService artistService;
+
+  @GetMapping(value = {"", "/"})
+  public ResponseEntity<List<Artist>> getAllArtists() {
+    List<Artist> artistList = artistService.getAllArtists();
+    return new ResponseEntity<>(artistList, HttpStatus.OK);
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<Artist> getArtistById(@PathVariable("id") long id) {
+    Artist artist = artistService.getArtistById(id)
+      .orElseThrow(() -> new ResponseStatusException(
+        HttpStatus.NOT_FOUND, "No Artist found with this ID", new ResourceNotFoundException()
+      ));
+    return new ResponseEntity<>(artist, HttpStatus.OK);
+  }
+}
